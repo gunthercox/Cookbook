193f2e7d0b67faa4afb81ffe749e3629b0695284
Gunther Cox <gunthercx@gmail.com>
2014-05-28 21:28:55 -0400

    Drafted the template generator

---
+++ waiter.py
@@ -0,0 +1,40 @@
+'''
+Run this file to generate the files for your cookbook.
+'''
+# import json
+
+print("Setting the table...")
+
+# for each directory in the recipies directory
+    # d = the directory name
+
+    # for each file in that directory
+        # get the json file that is d+'.json'
+
+            # get the title from the json file
+            # add title to markup
+
+            # get the description from the json file
+            # add description to markup
+
+            # for each item in the ingrediants
+                # get the item
+                # mark it up
+
+            # for each item in the skillsets
+                # get the item
+                # mark it up
+
+            # for each object in code:
+                # get the type
+                # mark it up
+
+                # get the content of the code file
+                # mark it up
+
+            # if an output directory doesn't exist, create it
+                # write the markup to a new page in the output directory
+
+
+# Done
+print("Bon appetit!")
